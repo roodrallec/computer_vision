f4765356779405e63255dea2394cbbd109b22025
JAlexander <roodrallec@users.noreply.github.com>
2017-10-11 12:12:14 +0200

    updated comment

--- mirror_image.m
+++ mirror_image.m
@@ -1,6 +1,5 @@
 function mirror = mirror_image(image, cut_col) 
-%MIRROR Take an image and return its mirror, reflected at the specified
-%cut column
+%MIRROR Take an image and return its mirror, reflected at the specified cut column
     width = length(image(1, :, 1));
     left = image(:,1:cut_col,:);
     right = image(:, cut_col:width, :);
