d6371b1dd672d23a9b1a1dda6ff37ce559be43c6
Jorge Alexander <jorgeNalexander1@gmail.com>
2017-10-22 23:24:02 +0200

    exercise_6 tasks complete

--- exercise6.m
+++ exercise6.m
@@ -9,6 +9,26 @@
 %- - - - - - 
 %Exercise 6:
 %- - - - - - 
-HAND= imread('images/hand.jpg');
-grayscale = rgb2gray(HAND);
-binary = grayscale;
+% 1
+HAND = imread('images/hand.jpg');
+MAPFRE = imread('images/mapfre.jpg');
+grayscaleHand = rgb2gray(HAND);
+% 2
+foregroundHand = grayscaleHand >= 50;
+backgroundHand = grayscaleHand < 50;
+reverseBinary = (foregroundHand - 1).^2;
+% note reverse binary == backgroundHand
+subplot(1,4,1), imshow(HAND);
+subplot(1,4,2), imshow(foregroundHand);
+subplot(1,4,3), imshow(backgroundHand);
+subplot(1,4,4), imshow(reverseBinary);
+
+% 3
+background = MAPFRE.*uint8(reverseBinary);
+foreground = HAND.*uint8(foregroundHand);
+handMapfre = background + foreground;
+subplot(1,1,1), imshow(handMapfre);
+
+% 4 
+imwrite(handMapfre, 'hand_mapfre.jpg');
+
