8968e977fe6a827933263d929967cb93e0d782e3
Emer Rodriguez Formisano <emer@emer.es>
2017-10-23 13:55:44 +0200

    Ex6 Reviewed

--- exercise6.m
+++ exercise6.m
@@ -1,7 +1,7 @@
 % - - - - - - 
 % MAI CV
 % Exercises Lab 1
-% Author name: Jorge Alexander
+% Author name: Emer Rodriguez Formisano and Jorge Alexander
 % - - - - - - 
 
 
@@ -9,26 +9,27 @@
 %- - - - - - 
 %Exercise 6:
 %- - - - - - 
-% 1
+% 1 Read Input
 HAND = imread('images/hand.jpg');
 MAPFRE = imread('images/mapfre.jpg');
 grayscaleHand = rgb2gray(HAND);
-% 2
-foregroundHand = grayscaleHand >= 50;
-backgroundHand = grayscaleHand < 50;
-reverseBinary = (foregroundHand - 1).^2;
-% note reverse binary == backgroundHand
-subplot(1,4,1), imshow(HAND);
-subplot(1,4,2), imshow(foregroundHand);
-subplot(1,4,3), imshow(backgroundHand);
-subplot(1,4,4), imshow(reverseBinary);
 
-% 3
-background = MAPFRE.*uint8(reverseBinary);
+% 2 Binarization
+th = 20;
+foregroundHand = grayscaleHand >= th;
+backgroundHand = not(foregroundHand);
+
+subplot(2,2,1), imshow(HAND), title("Original Hand");
+subplot(2,2,2), imshow(MAPFRE), title("Original Mapfre");
+subplot(2,2,3), imshow(foregroundHand), title("Foreground");
+subplot(2,2,4), imshow(backgroundHand), title("Background");
+
+% 3 Merge
+background = MAPFRE.*uint8(backgroundHand);
 foreground = HAND.*uint8(foregroundHand);
 handMapfre = background + foreground;
-subplot(1,1,1), imshow(handMapfre);
+subplot(1,1,1), imshow(handMapfre), title("Hand + Mafre");
 
-% 4 
+% 4 Save output
 imwrite(handMapfre, 'hand_mapfre.jpg');
 
