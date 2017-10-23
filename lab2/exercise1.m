74f622df8185cdb5078444ca88b51cbd48da2f28
Jorge Alexander <jorgeNalexander1@gmail.com>
2017-10-23 22:39:51 +0200

    exercise improvements

--- lab2/exercise1.m
+++ lab2/exercise1.m
@@ -17,32 +17,21 @@
 
 % Read the image "starbuck.jpg" and find its edges using matlab function
 % edge.
-% % Original image
-STARBUCK = imread('./images_video/starbuck.jpg');
-imshow(STARBUCK)
-% The traditional Starbucks logo, in color.
-% As a measure of the quality of the edge detection, 
-% the edges between the eyes of the mermaid in the image will be used.
-
-% % Matlab edge function with default parameters
-starbuckEdge = edge(STARBUCK(:,:,1));
-subplot(1,2,1), imshow(STARBUCK), title("Original image");
-subplot(1,2,2), imshow(starbuckEdge), title("Processed image");
-% The edges between the eyes are correctly defined. However,
-% discontinuities in the edges of some of the letters are observed, for
-% example, the letter A. A tuning of parameters is now tried to optimise
-% the edge etection.
-
-%% Parameter tuning
-% Apply different operators and find the optimal parameters for each of
-% them
-
+% Apply different operators (Sobel and Prewitt, Laplacian of Gaussian) and
+% find the optimal parameters for each of them.
+% Overlap the edges as in Fig.2
+% Repeat the experiment on 3 other images.
+for filename = {'./images_video/starbuck.jpg', './images_video/dog.jpg', './images_video/Einstein.jpg'}
+    showedges(filename{1});
+end
 % "In all cases, edge chooses the default threshold heuristically, depending on the input data. 
 % The best way to vary the threshold is to run edge once, capturing the calculated threshold as the second output argument. 
 % Then, starting from the value calculated by edge, adjust the threshold
 % higher (fewer edge pixels) or lower (more edge pixels)."
 % - MathWorks edge function documentation
 
+% % STARBUCK case study
+STARBUCK = imread('./images_video/starbuck.jpg');
 % Sobel
 % threshold = []; % return all edges that are stronger than threshold
 direction = 'both'; % specify the direction in which the function looks for edges in the image
@@ -97,31 +86,34 @@ subplot(1,2,1), imshow(STARBUCK), title("Original image");
 subplot(1,2,2), imshow(prewitt), title("Processed image");
 % The edge detection is optimised.
 
-
-sigma = 2;    % Scalar value that specifies the standard deviation of the Laplacian of Gaussian filter. 
-                % The default is 2. The size of the filter is n-by-n, where n=ceil(sigma*3)*2+1.
-[log, threshold] = edge(STARBUCK(:,:,1), 'log', [], sigma);
-[log, threshold] = edge(STARBUCK(:,:,1), 'log', threshold, sigma);
-subplot(1,1,1), imshow(log), title('Laplacian of Gaussian');
-
-roberts = edge(STARBUCK(:,:,1), 'Roberts');
-zerocross = edge(STARBUCK(:,:,1), 'zerocross');
-canny = edge(STARBUCK(:,:,1), 'Canny');
-
-subplot(2,3,1), imshow(sobel), title('Sobel');
-subplot(2,3,2), imshow(prewitt), title('Prewitt');
-subplot(2,3,4), imshow(log), title('Laplacian of Gaussian');
-
-subplot(2,3,3), imshow(roberts), title('Roberts');
-subplot(2,3,5), imshow(zerocross), title('Zerocross');
-subplot(2,3,6), imshow(canny), title('Canny');
-
-% How many methods to obtain the edges of the image are implemented by the function edge?
+% 1. How many methods to obtain the edges of the image are implemented by the function edge?
 % 6: Sobel, Prewitt, Laplacian of Gaussian, Roberts, Zerocross and Canny
 
-% Which is the best edge detector?
+% 2. Which is the best edge detector?
+% Depends on the image.
 
-% What are the advantages and disadvantages when extracting edges on the
-% different images?
+% 3. What are the advantages and disadvantages when extracting edges on the
+% different images? 
 
 % Discuss if parameters should be changed for different images.
+
+% "Gradient-based algorithms such as the Prewitt filter have a major drawback of being very sensitive to noise.
+% The size of the kernel filter and coefficients are fixed and cannot be adapted to a given image. An adaptive
+% edge-detection algorithm is necessary to provide a robust solution that is adaptable to the varying noise
+% levels of these images to help distinguish valid image contents from visual artifacts introduced by noise.
+
+% The performance of the Canny algorithm depends heavily on the adjustable parameters, ?, which is the
+% standard deviation for the Gaussian filter, and the threshold values, ?T1? and ?T2?. ? also controls the size of the
+% Gaussian filter. The bigger the value for ?, the larger the size of the Gaussian filter becomes. This implies more
+% blurring, necessary for noisy images, as well as detecting larger edges. As expected, however, the larger the
+% scale of the Gaussian, the less accurate is the localization of the edge. Smaller values of ? imply a smaller 
+% Gaussian filter which limits the amount of blurring, maintaining finer edges in the image. The user can tailor the
+% algorithm by adjusting these parameters to adapt to different environments.
+
+% Canny?s edge detection algorithm is computationally more expensive compared to Sobel, Prewitt and Robert?s
+% operator. However, the Canny?s edge detection algorithm performs better than all these operators under almost
+% all scenarios. Evaluation of the images showed that under noisy conditions, Canny, LoG, Sobel, Prewitt,
+% Roberts?s exhibit better performance, respectively."
+
+% - International Journal of Image Processing (IJIP), Volume (3) : Issue (1) 10
+%   Raman Maini & Dr. Himanshu Aggarwal
--- lab2/exercise2.m
+++ lab2/exercise2.m
@@ -9,29 +9,34 @@
 %- - - - - - 
 %Exercise 2: Enhancing images with edges
 %- - - - - - 
-% First the video object is loaded with the video file
+%% First the video object is loaded with the video file
 vidObj = VideoReader('images_video/Maldives.mp4');
 % Next the first frame is read and saved
 frame = readFrame(vidObj);
 imwrite(frame, 'frame_1.jpg');
 % The first frame is analysed using the showedges function that was defined
 showedges('frame_1.jpg');
-% Sobel is seen to give good edge detection    
-[sobel, thresh] = edge(rgb2gray(frame), 'Sobel', [], 'both', 'thinning');
-sobel = edge(rgb2gray(frame), 'Sobel', thresh, 'both', 'thinning');    
-highlightedFrame = overlapedges(frame, sobel);
+% Canny is seen to give good edge detection    
+sigma = 2;
+[canny, thresh] = edge(rgb2gray(frame), 'Canny', [], sigma);
+canny = edge(rgb2gray(frame), 'Canny', thresh);
+highlightedFrame = overlapedges(frame, canny);
 imshow(highlightedFrame);
-
-% % Here the video is edited and saved with highlighted edges 
+% Here the video is edited and saved with highlighted edges using Canny
+% edge detection with sigma=2 and thresh=[0.1250, 0.1625]
 reader = VideoReader('images_video/Maldives.mp4');
 writer = VideoWriter('images_video/maldivesContour.mp4', 'MPEG-4');
 writer.FrameRate = reader.FrameRate;
 open(writer);
+sigma = 2;
+thresh = [0.1250, 0.1625];
 while hasFrame(reader)
     frame = readFrame(reader);
-    [sobel, thresh] = edge(rgb2gray(frame), 'Sobel', [], 'both', 'thinning');
-    sobel = edge(rgb2gray(frame), 'Sobel', thresh, 'both', 'thinning');    
-    frame = overlapedges(frame, sobel);
+    canny = edge(rgb2gray(frame), 'Canny', thresh, sigma);
+    frame = overlapedges(frame, canny);
     writeVideo(writer, frame);
 end
 close(writer);
+% If we open the edited video, the edges of objects are observed more
+% clearly. This is particularly useful for identifying and generating
+% actions from the objects in the image, for example, car detection.
