135afd23b08db20591215bcbee6cb2f722da2bda
Jorge Alexander <jorgeNalexander1@gmail.com>
2017-11-16 13:46:40 +0100

    lab6

---
+++ EmerRodriguezFormisano+JorgeAlexander_CV_Lab6/main_subject_recognition.m
@@ -0,0 +1,15 @@
+% - - - - - - 
+% MAI CV
+% Exercises Lab 6
+% Author name: 
+% - - - - - - 
+% >> OBJECTIVE: 
+% 1)Complete this function to solve the subject recognition problem
+
+% main function
+function main_subject_recognition()
+
+end
+
+
+
