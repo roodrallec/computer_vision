64d3365b1ac55dbdc7779f25a010f0e4f3b7e1fa
Jorge Alexander <jorgeNalexander1@gmail.com>
2017-11-27 16:32:17 +0100

    food segmentation

--- EmerRodriguezFormisano+JorgeAlexander_CV_Lab8/levelset.m
+++ EmerRodriguezFormisano+JorgeAlexander_CV_Lab8/levelset.m
@@ -3,9 +3,9 @@ function f=levelset(f,im,mu,nu,c1,c2,lambda1,lambda2,kappa,g,tau) ;
 % CMP Vision Algorithms http://visionbook.felk.cvut.cz
 %
 % We implement a level set active contour segmentation method
-%  
+%
 % Warning: The level-set evolution formulation
-%  is 
+%  is
 % deceptively simple.  Robust and fast implementation of level set methods
 % on a discrete grid typically use special upwind update schemes with
 % frequent re-initialization to maintain the distance function property of
@@ -33,7 +33,7 @@ function f=levelset(f,im,mu,nu,c1,c2,lambda1,lambda2,kappa,g,tau) ;
 %     (outside) of the object to be segmented for the
 %     Chan-Vese area intensity criterion.
 %   lambda1  1x1
-%   lambda2  1x1  Coefficients lambda_1, lambda_2 
+%   lambda2  1x1  Coefficients lambda_1, lambda_2
 %     for the  Chan-Vese area intensity criterion.
 %   kappa  1x1  Coefficient kappa to penalize deviation of
 %     phi from the signed distance function.
@@ -47,12 +47,12 @@ function f=levelset(f,im,mu,nu,c1,c2,lambda1,lambda2,kappa,g,tau) ;
 %     unstable,  tau should be decreased.
 % Outputs:
 %   f  [m x n]  Final level set state phi.
-  
+
 % If needed, set the time step tau and initialize some variables;
-% areas will be used in the stopping criterion 
+% areas will be used in the stopping criterion
 % epsilon is the size of
 % the smoothed Dirac delta_varepsilon.
-  
+
 if nargin<11
   tau = 0.9/( abs(mu) + abs(nu) + abs(lambda1+lambda2) + abs(kappa) );
 end
@@ -64,7 +64,7 @@ end ;
 epsilon = 1.5;   i = 0;   areas = [];
 
 % We iterate in a while-loop until convergence. In the loop, we
-% ensure the correct boundary conditions (see NeumannBoundCond), 
+% ensure the correct boundary conditions (see NeumannBoundCond),
 % calculate the gradient and the
 % normalized gradient. eps guards against division by zero.
 % The Chan-Vese area intensity term fchvese does not depend on
@@ -76,7 +76,7 @@ while true
   normu = sqrt( ux.^2 + uy.^2 + eps );
   nx = ux ./ normu;  % normalized gradient
   ny = uy ./ normu;
-% All parts of the right-hand side of the evolution 
+% All parts of the right-hand side of the evolution
 % equation (??) are evaluated:
 % the distance function deviation term fdist and the curve and area
 % terms fcurvearea. We assemble an update using a smoothed Dirac
@@ -91,18 +91,18 @@ while true
 % The stopping criterion is as in Section ??. In addition,
 % we stop after a fixed number of iterations.
   area  = sum( f(:)<0 );
-  areas = [areas area]; 
+  areas = [areas area];
   if length(areas)>10
     areas = areas(end-9:end);  % keep last 10 areas
     if max(areas)-min(areas)<1 || i>1000, break; end
   end
 
   if rem(i,50)==0,
-    figure(1) ; subplot(121) ; imagesc(im) ; colormap(gray) ; 
-    axis equal ; hold on ; 
+    figure(1) ; subplot(121) ; imagesc(im) ; colormap(gray) ;
+    axis equal ; hold on ;
     contour(f,[0 0],'r') ; hold off ; colorbar ;
-    subplot(122) ; imagesc(f,[-100 100]) ; 
-    axis equal ; colormap(jet) ; hold on ; 
+    subplot(122) ; imagesc(f,[-100 100]) ;
+    axis equal ; colormap(jet) ; hold on ;
     contour(f,[0 0],'k') ; hold off ; colorbar ;
 %    subplot(223) ; plot(f(75,:)) ; grid on ;
 %    subplot(224) ; plot(step*fu(75,:)) ; grid on ;
@@ -129,7 +129,6 @@ function div=divergence(x,y);
 
 function f = NeumannBoundCond(f)
 [nrow,ncol] = size(f);
-f([1 nrow],[1 ncol]) = f( [3 nrow-2], [3 ncol-2] );  
-f([1 nrow],2:end-1)  = f( [3 nrow-2], 2:end-1 );          
-f(2:end-1,[1 ncol])  = f( 2:end-1, [3 ncol-2] );          
- 
+f([1 nrow],[1 ncol]) = f( [3 nrow-2], [3 ncol-2] );
+f([1 nrow],2:end-1)  = f( [3 nrow-2], 2:end-1 );
+f(2:end-1,[1 ncol])  = f( 2:end-1, [3 ncol-2] );
---
+++ EmerRodriguezFormisano+JorgeAlexander_CV_Lab8/segmentation_food.m
@@ -0,0 +1,25 @@
+%% Reading the image
+col_img = imread('images/dishes/PB070009.JPG');
+col_img = imresize(col_img, 0.25);
+img = im2double(col_img);
+img = rgb2gray(img);
+%% Building phi initial contour
+[nx,ny] = size(img);
+[x,y] = meshgrid(1:nx,1:ny);
+% Phi
+f = sqrt( (x-70).^2 + (y-70).^2 ) - 5;
+f = f';
+f = f(1:nx, 1:ny);
+g = ones(nx, ny);
+%% Displaying the initial position
+figure(1);
+% Image with contour overlaid
+subplot(2,1,1); imagesc(col_img); hold on; axis equal;
+contour(f,[0,0],'r','LineWidth',2); hold off;
+%% Performing the segmentation
+% f=levelset(f,im,mu,nu,c1,c2,lambda1,lambda2,kappa,g,tau) ;
+f1=levelset(f,img,100,0,10,70,1,1,0.05,g,1);
+% f1=levelset(f,img,500,0,30,70,0.5,0.5,0.1,g,1) ;
+%% plotting the final result
+subplot(2,1,2); imagesc(col_img); hold on; axis equal;
+contour(f1,[0,0],'r','LineWidth',2); hold off;
