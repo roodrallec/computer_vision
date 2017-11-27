2c182087096a47c9590f64f66f07078c474daf12
Jorge Alexander <jorgeNalexander1@gmail.com>
2017-11-27 17:37:26 +0100

    fixed food segmentation

--- EmerRodriguezFormisano+JorgeAlexander_CV_Lab8/segmentation_food.m
+++ EmerRodriguezFormisano+JorgeAlexander_CV_Lab8/segmentation_food.m
@@ -1,8 +1,8 @@
 %% Reading the image
 col_img = imread('images/dishes/PB070009.JPG');
 col_img = imresize(col_img, 0.25);
-img = im2double(col_img);
-img = rgb2gray(img);
+img = rgb2gray(uint8(col_img));
+img = double(img);
 %% Building phi initial contour
 [nx,ny] = size(img);
 [x,y] = meshgrid(1:nx,1:ny);
@@ -18,7 +18,7 @@ subplot(2,1,1); imagesc(col_img); hold on; axis equal;
 contour(f,[0,0],'r','LineWidth',2); hold off;
 %% Performing the segmentation
 % f=levelset(f,im,mu,nu,c1,c2,lambda1,lambda2,kappa,g,tau) ;
-f1=levelset(f,img,100,10,3,10,0.5,0.5,0.07,g,1) ;
+f1=levelset(f,img,500,0,100,200,0.5,0.5,0.05,g,1) ;
 %% plotting the final result
 subplot(2,1,2); imagesc(col_img); hold on; axis equal;
 contour(f1,[0,0],'r','LineWidth',2); hold off;
