a6aec1a7105a5f6594acafd55b55ac908974bcc0
emerrf <emer@emer.es>
2017-12-19 11:22:25 +0100

    current state of lab11

---
+++ EmerRodriguezFormisano+JorgeAlexander_CV_Lab11/Lab11R.m
@@ -0,0 +1,14 @@
+
+
+% Note for Jorge, we need to use versio 0.9.20 as the code we have to study
+% was modified from version 0.9.16 used in the previous assignment
+%% Load VLFeat
+run('vlfeat-0.9.20/toolbox/vl_setup')
+vl_version
+
+%% 
+run('vlfeat-0.9.20/apps/phow_caltech101.m')
+vl_version
+
+function 
+
